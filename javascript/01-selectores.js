6446ad4774c11835d42e08dbb19aff72abbbaaf0
Renato549 <renatoramirezcalva@gmail.com>
2026-02-12 12:59:33 -0600

    Prueba con selectores

---
+++ javascript/01-selectores.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+    // console.log("Estamos listos");
+    var rojo = $("#rojo")
+    rojo.css("background", "red")
+        .css("color","white");
+    var azul = $("#azul")
+    azul.css("background", "azul")
+        .css("color","white");
+    var verde = $("#verde")
+    verde.css("background", "green")
+        .css("color","white");
+});
